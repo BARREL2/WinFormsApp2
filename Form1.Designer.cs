a5344343a3b88badfa562e77b7da877c0e723244
BARREL2 <barrelc12@gmail.com>
2023-02-07 22:10:50 +0900

    1つのイベントで3ボタンそれぞれを制御するsenderダウンキャスト

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,22 +43,77 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.UpdateTextBox);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckKeyPress);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(117, 162);
+            this.button1.Location = new System.Drawing.Point(131, 148);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 105);
+            this.button1.Size = new System.Drawing.Size(71, 26);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.HandleButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.HandleButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(131, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.HandleButtonClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.UpdateTextBox);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckKeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(131, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.UpdateTextBox);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckKeyPress);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Location = new System.Drawing.Point(287, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 117);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 327);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,5 +127,10 @@
 
         private TextBox textBox1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,48 @@
 namespace WinFormsApp2
 {
-    public interface IForm
+    public partial class Form1 : Form
     {
-        string TextBoxValue { get; set; }
-    }
+        private readonly Dictionary<string, bool> _textBoxValues = new Dictionary<string, bool>
+        {
+            { "textBox1", false },
+            { "textBox2", false },
+            { "textBox3", false }
+        };
 
-    public partial class Form1 : Form, IForm
-    {
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.KeyPress += CheckKeyPress;
+            textBox2.KeyPress += CheckKeyPress;
+            textBox3.KeyPress += CheckKeyPress;
         }
 
-        public string TextBoxValue { get; set; }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void HandleButtonClick(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2(this);
-            form2.Show();
+            Button button = (Button)sender;
+            button.BackColor = Color.LightGreen;
+        }
+
+        private void UpdateTextBox(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            _textBoxValues[textBox.Name] = !string.IsNullOrEmpty(textBox.Text);
+
+            button4.BackColor = _textBoxValues.Values.All(value => value)
+                ? Color.GreenYellow
+                : SystemColors.ControlLight;
+        }
+
+        private void CheckKeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox == null)
+                return;
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
         }
     }
 }
